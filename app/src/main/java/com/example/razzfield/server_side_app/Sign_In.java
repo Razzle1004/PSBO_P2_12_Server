f50c0194fe7b61b262ec1e27cb50b0c450de9dc9
Razzfield <Razzfield@gmail.com>
2019-05-19 23:35:11 +0700

    Inisialisasi Sign-in page sisi server

---
+++ app/src/main/java/com/example/razzfield/server_side_app/Sign_In.java
@@ -0,0 +1,13 @@
+package com.example.razzfield.server_side_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__in);
+    }
+}
